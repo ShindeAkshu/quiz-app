141ffd293943945bbd415bc8e242c4855828501b
shindeAkshu <sakshata416@gmail.com>
2022-07-19 10:18:44 -0700

    Saved  All Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
  
 import './App.css';
 import React from 'react';
-
-
+import { useState } from 'react';
+import MainMenu from './components/MainMenu';
+import Quiz from './components/Quiz';
+import EndScreen from './components/EndScreen'
+import {QuizContext} from './Helpers/Context'
 
 function App() {
+const[gameState,setGameState] =useState("menu");
+
   return (
     <div className="App">
-      
+      <h1>Quiz App</h1>
+      {gameState === "menu" && <MainMenu/>}
+      {gameState === "quiz" && <Quiz/>}
+      {gameState === "endscreen" && <EndScreen/>}
     </div>
   );
 }
---
+++ src/components/MainMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function MainMenu () {
+    return(
+        <div className='menu'>
+            <button>Start Quiz</button>
+          
+        </div>
+    );
+}
+
+export default MainMenu;
