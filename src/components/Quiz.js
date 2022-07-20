e970cd7e2616d3011a04b48fc3a77a99d10b9924
shindeAkshu <sakshata416@gmail.com>
2022-07-20 12:36:32 -0700

    modified

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {useState} from 'react';
+import {Questions} from "./Helpers/QuestionBank";
+
 
 function Quiz () {
+  const[currQuestion,setCurrQuestion] = useState(0);
+  const[optionChosen,setOptionChosen] =useState("");
+
 return(
+
     <div className="quiz">
-      Quiz
+  <h1>{Questions[currQuestion].prompt}</h1>
+  <div className="option">
+    <button>{Questions[currQuestion].optionA}</button>
+    <button>{Questions[currQuestion].optionB}</button>
+    <button>{Questions[currQuestion].optionC}</button>
+    <button>{Questions[currQuestion].optionD}</button>
+   </div>
+   <button>Next QuestionBank</button>
     </div>
     );
 };
